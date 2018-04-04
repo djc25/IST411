00be5642c95332b3139c3958f0aae7a9b916659b
jss5783 <>
2018-04-04 16:23:42 -0400

    Beginning of hand-coded GUI, since teacher and I can't get the GUI-builder-built one working.

--- src/foursquare/Foursquare.java
+++ src/foursquare/Foursquare.java
@@ -5,11 +5,52 @@
  */
 package foursquare;
 
+import java.awt.CardLayout;
+import java.awt.Color;
+import java.awt.Container;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 /**
  *
  * @author Jason
  */
 public class Foursquare {
+    
+    public Foursquare()
+    {
+        JFrame jfClientMain = new JFrame("Project 4Square (CLIENT)");
+        JPanel jpClientLogIn = new JPanel();
+        JPanel jpClientLobby = new JPanel();
+        JPanel jpClientMatch = new JPanel();
+        JFrame jfServerMain = new JFrame("Project 4Square (SERVER)");
+        CardLayout cl = new CardLayout();
+        
+        Container cClient = jfClientMain.getContentPane();
+        cClient.setLayout(cl);
+//         System.out.println("Content pane layout class - " + cClient.getLayout().getClass().getName() );
+      //  jfClientMain.setLayout(new CardLayout());
+      //    System.out.println("Frame layout class - " + jfClientMain.getLayout().getClass().getName());
+        jfClientMain.setSize(960, 540);
+        jpClientLogIn.setSize(200, 100);
+        jpClientLogIn.setBackground(Color.RED);
+        jpClientLobby.setSize(200, 100);
+        jpClientLobby.setBackground(Color.YELLOW);
+        jpClientMatch.setSize(200, 100);
+        jpClientMatch.setBackground(Color.BLUE);
+        jfClientMain.add(jpClientLogIn);
+        jfClientMain.add(jpClientLobby);
+        jfClientMain.add(jpClientMatch);
+        
+        jfClientMain.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jfClientMain.setVisible(true);
+//        cl.show(jfClientMain, jpClientLobby.toString() );
+        cl.next(cClient);
+        cl.next(cClient);
+      
+      //CardLayout myCL = (CardLayout)jfClientMain.getLayout();
+      //myCL.next(jfClientMain);
+    }
 
     /**
      * @param args the command line arguments
@@ -17,6 +58,8 @@ public class Foursquare {
     public static void main(String[] args) {
 //        jfClientLobby jfl = new jfClientLobby();
 //        jfl.setVisible(true);
+
+        Foursquare fs = new Foursquare();
     }
     
 }
