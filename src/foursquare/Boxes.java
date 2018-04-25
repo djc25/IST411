cd11bc7131673a68810606ab87ba93d4eb85bb8d
jss5783 <>
2018-04-25 15:16:16 -0400

    Added more comments in Boxes.

--- src/foursquare/Boxes.java
+++ src/foursquare/Boxes.java
@@ -14,17 +14,19 @@ import java.awt.Graphics;
  * @author DRACOX
  * 
  * ----------CHANGELOG----------
+ * 2018/04/25 -     Continued adding comments to comply with coding conventions. -JSS5783
+ * 
  * 2018/04/23 -     Started renaming variables and adding comments to comply with coding conventions. -JSS5783
  */
 
 
 
-public class Boxes {
-    
+public class Boxes
+{
     /*
         A Box knows the status (true [checked] /false [unchecked]) of its own four sides (left, right, up, and down).
         It also knows which lines it's connected to (Lines) and their relative positions (left, right, up, and down).
-        Finally, it knows whether or not it's closed (i.e., all four sides are closed.
+        Finally, it knows whether or not it's closed (i.e., all four sides are closed).
     */
     
     //declaration
@@ -50,128 +52,158 @@ public class Boxes {
         //initialization
         bTopSide = bBottomSide = bLeftSide = bRightSide = false;
         bIsClosed = false;
-    }
+    }   //END Boxes()
 
     
     
     /**
-     * @return bTopSide - returns true if the Box's top side is checked, and false if not.
+     * Returns true if the Box's top side is checked, and false if not.
+     * @return bTopSide
      */
-    public boolean isTop() {
+    public boolean isTop()
+    {
         return bTopSide;
-    }
+    }   //END isTop()
 
     /**
-     * @param bInTopLine - sets the Box's top side to checked if true, and unchecked if false.
+     * Sets the Box's top side to checked if true, and unchecked if false.
+     * @param bInTopLine
      */
-    public void setTop(boolean bInTopLine) {
+    public void setTop(boolean bInTopLine)
+    {
         bTopSide = bInTopLine;
-    }
+    }   //END setTop(boolean bInTopLine)
 
     
     
     /**
-     * @return bBottomSide - returns true if the Box's bottom side is checked, and false if not.
+     * Returns true if the Box's bottom side is checked, and false if not.
+     * @return bBottomSide
      */
-    public boolean isBot() {
+    public boolean isBot()
+    {
         return bBottomSide;
-    }
+    }   //END isBot()
 
     /**
-     * @param bInBottomLine - sets the Box's bottom side to checked if true, and unchecked if false.
+     * Sets the Box's bottom side to checked if true, and unchecked if false.
+     * @param bInBottomLine
      */
-    public void setBot(boolean bInBottomLine) {
+    public void setBot(boolean bInBottomLine)
+    {
         bBottomSide = bInBottomLine;
-    }
+    }   //END setBot(boolean bInBottomLine)
 
     
     
     /**
-     * @return bLeftSide - returns true if the Box's left side is checked, and false if not.
+     * Returns true if the Box's left side is checked, and false if not.
+     * @return bLeftSide
      */
-    public boolean isLeft() {
+    public boolean isLeft()
+    {
         return bLeftSide;
-    }
+    }   //END isLeft()
 
     /**
-     * @param bInLeftLine - sets the Box's left side to checked if true, and unchecked if false.
+     * Sets the Box's left side to checked if true, and unchecked if false.
+     * @param bInLeftLine
      */
-    public void setLeft(boolean bInLeftLine) {
+    public void setLeft(boolean bInLeftLine) 
+    {
         bLeftSide = bInLeftLine;
-    }
+    }   //END setLeft(boolean bInLeftLine)
 
     
     
     /**
-     * @return bRightSide - returns true if the Box's right side is checked, and false if not.
+     * Returns true if the Box's right side is checked, and false if not.
+     * @return bRightSide
      */
-    public boolean isRight() {
+    public boolean isRight()
+    {
         return bRightSide;
-    }
+    }   //END isRight()
 
     /**
-     * @param bInRightLine - sets the Box's right side to checked if true, and unchecked if false.
+     * Sets the Box's right side to checked if true, and unchecked if false.
+     * @param bInRightLine
      */
-    public void setRight(boolean bInRightLine) {
+    public void setRight(boolean bInRightLine)
+    {
         bRightSide = bInRightLine;
-    }
+    }   //END setRight(boolean bInRightLine)
 
     
     
     /**
-     * @return the lineTop
+     * Gets the game grid Line that is stored as the Box's top line.
+     * @return lineTop
      */
-    public Lines getTopLine() {
+    public Lines getTopLine()
+    {
         return lineTop;
-    }
+    }   //END getTopLine()
 
     /**
+     * Sets the Box's top line to the Line passed from the game grid.
      * @param lineInTop - the Line that the Box's top line should be set to.
      */
-    public void setTopLine(Lines lineInTop) {
+    public void setTopLine(Lines lineInTop)
+    {
         lineTop = lineInTop;
-    }
+    }   //END setTopLine(Lines lineInTop)
 
     
     
     /**
-     * @return the lineBottom
+     * Gets the game grid Line that is stored as the Box's bottom line.
+     * @return lineBottom
      */
-    public Lines getBotLine() {
+    public Lines getBotLine()
+    {
         return lineBottom;
-    }
+    }   //END getBotLine()
 
     /**
+     * Sets the Box's bottom line to the Line passed from the game grid.
      * @param lineInBottom - the Line that the Box's bottom line should be set to.
      */
-    public void setBotLine(Lines lineInBottom) {
+    public void setBotLine(Lines lineInBottom) 
+    {
         lineBottom = lineInBottom;
-    }
+    }   //END setBotLine(Lines lineInBottom)
 
     
     
     /**
-     * @return the lineLeft
+     * Gets the game grid Line that is stored as the Box's left line.
+     * @return lineLeft
      */
-    public Lines getLeftLine() {
+    public Lines getLeftLine()
+    {
         return lineLeft;
-    }
+    }   //END getLeftLine()
 
     /**
+     * Sets the Box's left line to the Line passed from the game grid.
      * @param lineInLeft - the Line that the Box's left line should be set to.
      */
-    public void setLeftLine(Lines lineInLeft) {
+    public void setLeftLine(Lines lineInLeft)
+    {
         lineLeft = lineInLeft;
-    }
+    }   //END setLeftLine(Lines lineInLeft)
 
     
     
     /**
-     * @return the lineRight
+     * Gets the game grid Line that is stored as the Box's right line.
+     * @return lineRight
      */
-    public Lines getRightLine() {
+    public Lines getRightLine()
+    {
         return lineRight;
-    }
+    }   //END getRightLine()
 
     /**
      * @param lineInRight - the Line that the Box's right line should be set to.
@@ -183,12 +215,13 @@ public class Boxes {
     
     
     /**
-     * @return bIsClosed - returns true if the Box is closed, and false if it is not.
+     * Returns true if the Box is closed, and false if it is not.
+     * @return bIsClosed
      */
     public boolean getClosed()
     {
         return bIsClosed;
-    }
+    }   //END getClosed()
     
     /**
      * Checks if the Box is closed.
@@ -200,7 +233,7 @@ public class Boxes {
     public void isClosed(Graphics g)
     {
         
-        if(lineTop.isIsConnected()&&lineBottom.isIsConnected()&&lineLeft.isIsConnected()&&lineRight.isIsConnected())
+        if(lineTop.isIsConnected() && lineBottom.isIsConnected() && lineLeft.isIsConnected() && lineRight.isIsConnected() )   //if all lines are checked
         {
             g.fillRect(lineTop.getxStart(), lineTop.getyStart(), 100, 100);
             bIsClosed = true;
@@ -210,7 +243,8 @@ public class Boxes {
         {
             bIsClosed = false;
         }
-    }
+    }   //END isClosed(Graphics g)
     
     
-}
+    
+}   //END Boxes
